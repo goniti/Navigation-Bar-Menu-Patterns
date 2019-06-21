65a15e8e04b139fef35fff87fd2e8779d07854c2
Carlos <goniti@live.fr>
2019-06-21 12:39:03 +0200

    Add burger menu close click outside

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,43 @@
-var navOpenBtn = document.querySelector('.burgerMenu__btn--open');
-var navCloseBtn = document.querySelector('.burgerMenu__btn--close');
-var nav = document.querySelector('.burgerMenu');
-var pageContent = document.querySelector('.page__content');
-var navList = document.querySelector('.burgerMenu__list');
-var page = document.querySelector('body');
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(document).scrollTop() >= 100) {
+      //value >= 100 (1 scroll)
+      //value > 100 (2 scroll)
 
-//open nav
-navOpenBtn.addEventListener('click', function () {
-  navOpenBtn.classList.add('js-hidden');
+      $(".navbar").addClass("navbar__menu-scrolled");
+    } else {
+      $(".navbar").removeClass("navbar__menu-scrolled");
+    }
+  });
 
-  nav.classList.add('js-opened');
+  //BURGER-MENU
+  const burgerMenu = $('.burgerMenu');
+  const burgerMenuBtnOpen = $('.burgerMenu__btn--open');
+  const burgerMenuBtnClose = $('.burgerMenu__btn--close');
+  const page = $('.page__content');
 
-  pageContent.classList.add('js-opened');
+  // Open burger menu
+  $(burgerMenuBtnOpen).on('click', function () {
+    $(burgerMenuBtnOpen).addClass('js-hidden');
+    $(burgerMenu).addClass('js-opened');
+    $(page).addClass('js-opened');
+
+  });
+
+  // Close burger menu
+  $(burgerMenuBtnClose).on('click', function () {
+    $(burgerMenuBtnOpen).removeClass('js-hidden');
+    $(burgerMenu).removeClass('js-opened');
+    $(page).removeClass('js-opened');
+  });
+  // Click outside close burger menu
+  $(page).on("click", function () {
+    $(burgerMenuBtnOpen).removeClass('js-hidden');
+    $(burgerMenu).removeClass('js-opened');
+    $(page).removeClass('js-opened');
+  });
+
+
 });
 
-//close nav
-navCloseBtn.addEventListener('click', function () {
-  navOpenBtn.classList.remove('js-hidden');
 
-  nav.classList.remove('js-opened');
-
-  pageContent.classList.remove('js-opened');
-});
-
-//closing navigation if click outside it
-page.addEventListener('click', function (e) {
-
-  var evTarget = e.target;
-
-  if ((evTarget !== nav) && (nav.classList.contains('js-opened')) && (evTarget !== navOpenBtn) && (evTarget.parentNode !== navOpenBtn)) {
-    console.log(navOpenBtn.firstChild);
-
-    navOpenBtn.classList.remove('js-hidden');
-
-    nav.classList.remove('js-opened');
-
-    pageContent.classList.remove('js-opened');
-  }
-
-});
